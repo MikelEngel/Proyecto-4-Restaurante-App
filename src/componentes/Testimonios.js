2dbb2d36c7ebf77e3d80056f5a6b2130f45b2c69
MikelEngel <angelhevel@gmail.com>
2022-08-30 17:23:20 -0500

    testimonios

--- src/componentes/Testimonios.js
+++ src/componentes/Testimonios.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio () {
+function Testimonio (props) {
   return (
     <div className='contenedor-testimonio'>
 
       <img 
       className='imagen-testimonio'
-      src={require('../imagenes/testimonio-joven.png')}
-      alt='foto joven' />
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt='foto' />
+
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> - servicio - <strong>{props.alimento}</strong></p>
+        <p className='frase-testimonio'><strong>{props.frase}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+
+      </div>
 
     </div>
   )
 }
+
+export default Testimonio
+
