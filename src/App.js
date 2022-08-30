b96ec6f5c556334644cdd1ec6eef4ef0062b043a
MikelEngel <angelhevel@gmail.com>
2022-08-30 18:54:23 -0500

    barra de navegación

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonios'
+import Formulario from './componentes/Formulario'
+import React, {useEffect} from 'react';
+import db from './firebase/firebaseConfig';
+import { collection, getDocs } from "firebase/firestore";
+import Footer from './componentes/Footer';
+
 
 function App() {
+
+  useEffect (()=> {
+
+    const obtenerDatos = async() => {
+      const datos = await getDocs(collection(db, 'usuarios'));
+      datos.forEach((documento) =>{
+        console.log(documento.data());
+      });
+
+    }
+
+    obtenerDatos();
+    
+  }, []);   
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='contenedor-principal'>
+      <h1>Esto dicen nuestros comensales sobre nuestros platillos</h1>
+      <Testimonio 
+      imagen='joven'
+      nombre='Jonh' 
+      alimento='Cena'
+      frase='Bonita Velada'
+      testimonio='Escojas la cena que escojas siempre será un acierto, un trato muy servicial, amable y con rapidez' />
+
+      <Testimonio 
+      imagen='mujer'
+      nombre='Rose' 
+      alimento='Comida'
+      frase='Riquisimo'
+      testimonio='El menú es excelente y 100% recomendable, el servicio rápido la gente amable y la comida exquisita! está increíble' />
+
+     <Testimonio 
+      imagen='senor'
+      nombre='Steve' 
+      alimento='Desayuno'
+      frase='Almuerzo de negocios'
+      testimonio='Cada uno tenía un plato de especialidad que era excelente y el mesero fue muy atento con nostros, nos hizo sentir bienvenidos. ' />
+      </div>
+
+      <div>
+        <h2>HAZ TU RESERVACIÓN</h2>
+        <Formulario />
+      </div>
+
     </div>
+
   );
 }
 
---
+++ src/componentes/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer () {
+   return(
+
+    <div className='contenedor-footer'>
+
+        <footer>
+            <h3>Footer</h3>
+        </footer>
+
+    </div>
+
+   )
+      
+}
+
+export default Footer
