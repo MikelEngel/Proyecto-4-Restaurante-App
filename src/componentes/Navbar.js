7fcdf9ba375a6ecd586592e36d0ffc50d12ce497
MikelEngel <angelhevel@gmail.com>
2022-09-01 14:55:25 -0500

    detalles en barra de navegación

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -19,8 +19,8 @@ const Navbar = () => {
         </li>
       </ul>
       <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <input className="form-control me-2" type="search" placeholder="Escribe tu búsqueda" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
