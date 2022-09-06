cba3c4077e0802303ca31fb7715f8285a858648f
MikelEngel <angelhevel@gmail.com>
2022-09-06 00:15:35 -0500

    footer agregado

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
-function Footer () {
-   return(
+const Footer = () => {
 
-    <div className='contenedor-footer'>
+    return (
 
-        <footer>
-            <h3>Footer</h3>
-        </footer>
+        <div>
+    <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top bg-warning">
+        <div className="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-white text-decoration-none lh-1">
+        
+        </a>
+        <span className="mb-3 mb-md-0 text-black">&copy; 2022 Restaurante Platillos Mexicanos</span>
+        </div>
 
-    </div>
+    </footer>
+        </div>
 
-   )
+    )
       
 }
 
